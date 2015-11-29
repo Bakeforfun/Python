b15760f4972b51010ae3ad02c2834b81d338a239
Bakeforfun <bakeforfun@gmail.com>
2015-11-29 23:12:52 +0300

    Exercises 1 - 33 (+26 text)

---
+++ PyAlgoTrade/1_print_prices.py
@@ -0,0 +1,24 @@
+# Download data by typing in the terminal in one line
+# python -c "from pyalgotrade.tools import yahoofinance;
+# yahoofinance.download_daily_bars('orcl', 2000, 'orcl-2000.csv')"
+
+from pyalgotrade import strategy
+from pyalgotrade.barfeed import yahoofeed
+
+
+class MyStrategy(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+
+    def onBars(self, bars):
+        bar = bars[self.__instrument]
+        self.info(bar.getClose())
+
+# Load the yahoo feed from the CSV file
+feed = yahoofeed.Feed()
+feed.addBarsFromCSV("orcl", "orcl-2000.csv")
+
+# Evaluate the strategy with the feed's bars.
+myStrategy = MyStrategy(feed, "orcl")
+myStrategy.run()
